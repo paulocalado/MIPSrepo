382a0bf67a421c936fb932f44817952c0dac6563
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-05-15 15:34:44 -0300

    lendo uma string

---
+++ readingString.asm
@@ -0,0 +1,43 @@
+.data
+ message: .asciiz "\nPaulo Henrique"
+ key: .word 1
+ file:    .asciiz "string.in"
+    buffer:  .space 1024
+    error:   .ascii "Arquivo não encontrado"
+.text
+main:
+ # open a file
+        li $v0, 13    #open file
+        la $a0, file  
+        li $a1, 0     # flag for read only 0 = read; 1 write/create; 9 write/create/append
+        li $a2, 0     # flag for ignore
+        syscall       # open a file (file descriptor returned in $v0) 
+    
+    
+        move $s0, $v0   # file descriptor saved in $s0
+        
+
+        # read file
+        li   $v0, 14        #code to read file
+        add  $a0, $s0, $0	
+        la   $a1, buffer    # address of buffer from which to read
+        li   $a2,  1024     # hardcoded buffer length
+        syscall             # read from file
+encryptMessage:
+    la $s0, buffer     #s0 will hold message that will be iterated through
+    lw $t1, key     #s1 will hold the key to shift by
+    li $t0, 0       #t0 will be iterator, starting at 0
+ 
+encryptionLoop:
+    add $s1, $s0, $t0   #$s1 = message[i]
+    lb $s2, 0($s1)      #Loading char to shift into $s2
+    beq $s2, $zero, exit    #Breaking the loop if we've reached the end: http://stackoverflow.com/questions/12739463/how-to-iterate-a-string-in-mips-assembly
+    add $s2, $s2, $t1   #Shifting the character by the key amount
+    #la $s1, ($s2)       #Changing the character in message to the shifted character
+    addi $t0, $t0, 1    #i++
+    j encryptionLoop    #Going back to the beginning of the loop
+    
+  exit:
+  li $v0, 1
+  move $a0, $t0
+  syscall  
